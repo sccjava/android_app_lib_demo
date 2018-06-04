3d47d3d6b181e65c4d0a026bcafcf186c50521a3
harman-John <John.Gan@harman.com>
2018-06-04 14:09:13 +0800

    config gradle when using external library for building app.

--- SlaveProject/sp_lib/src/main/java/com/bfrx/Device.java
+++ SlaveProject/sp_lib/src/main/java/com/bfrx/Device.java
@@ -1,7 +1,17 @@
 
 package com.bfrx;
 
-public class Device implements Serializable{
+import android.content.Context;
+
+import com.media.player.R;
+
+import java.io.Serializable;
+
+public class Device implements Serializable {
 	public long uniqueID = 0;
 	public int protocol = 0;
+
+	public void setDevice(Context context){
+		context.getColor(R.color.colorAccent);
+	}
 }
